8396ea0be217d810107c5fcc5234438f70e3852f
RockChang <peng0556@qq.com>
2021-12-21 17:25:03 +0800

    test: jenkins

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Peng zhang
  * @Date: 2021-02-03 21:21:54
- * @LastEditTime: 2021-12-21 14:21:29
+ * @LastEditTime: 2021-12-21 17:24:53
  * @Description: 配置文件
  */
 
@@ -33,7 +33,7 @@ export const ENV = 'dev';
 // 服务器配置
 export const SERVICE = {
   HOST: '',
-  PORT: '50000',
+  PORT: '3000',
 };
 // 接口地址配置
 export const API = {
